2da7f18aca3ce1efeeace293fb3b21d0a5398ac6
Krystian Ogonowski <tailowskikrystian@gmail.com>
2022-10-13 23:11:08 +0200

    added django-extensions

--- api/settings.py
+++ api/settings.py
@@ -60,6 +60,23 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "PatientRegistrySystem.urls"
+ROOT_URLCONF = "api.urls"
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 WSGI_APPLICATION = "api.wsgi.application"
 
